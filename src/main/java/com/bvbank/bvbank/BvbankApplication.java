27582fb637b99a6969ff07ea08700159d4395b4f
HVQuan02 <hovquan02@gmail.com>
2025-09-25 16:26:01 +0700

    Setup

---
+++ src/main/java/com/bvbank/bvbank/BvbankApplication.java
@@ -0,0 +1,13 @@
+package com.bvbank.bvbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvbankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bvbank/bvbank/HelloController.java
@@ -0,0 +1,13 @@
+package com.bvbank.bvbank; // Đổi thành package name thật
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
